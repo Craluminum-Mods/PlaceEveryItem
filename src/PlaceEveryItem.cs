daa0a661aacf43f773311ca6864dfc0d9e08be4f
Craluminum2413 <beware.14b@gmail.com>
2023-05-19 22:09:37 +0300

    inline LoadObjectFromJson method

--- src/PlaceEveryItem.cs
+++ src/PlaceEveryItem.cs
@@ -1,8 +1,6 @@
 using Vintagestory.API.Common;
 using PlaceEveryItem.Configuration;
 using System.Collections.Generic;
-using Vintagestory.API.Datastructures;
-using Newtonsoft.Json.Linq;
 
 [assembly: ModInfo("Place Every Item",
     Authors = new[] { "Craluminum2413" })]
@@ -23,13 +21,7 @@ namespace PlaceEveryItem
 
         public override void AssetsLoaded(ICoreAPI api)
         {
-            transformations = LoadObjectFromJson<Dictionary<string, ModelTransform>>(api, "pei:config/transformations.json");
-        }
-
-        private T LoadObjectFromJson<T>(ICoreAPI api, string path)
-        {
-            var jsonObject = new JsonObject(api.Assets.Get<JToken>(new AssetLocation(path)));
-            return jsonObject.Token.ToObject<T>();
+            transformations = api.Assets.Get(new AssetLocation("pei:config/transformations.json")).ToObject<Dictionary<string, ModelTransform>>();
         }
     }
 }
