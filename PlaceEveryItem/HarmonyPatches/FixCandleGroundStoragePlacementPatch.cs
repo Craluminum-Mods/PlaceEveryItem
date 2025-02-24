dc8d833380d92ee92c3a11ea012192f85c6bf623
Dana <beware.14b@gmail.com>
2025-02-24 19:15:41 +0200

    fix candle placement

---
+++ PlaceEveryItem/HarmonyPatches/FixCandleGroundStoragePlacementPatch.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+
+namespace PlaceEveryItem;
+
+[HarmonyPatch(typeof(ItemCandle), nameof(ItemCandle.OnHeldInteractStart))]
+public static class FixCandleGroundStoragePlacementPatch
+{
+    [HarmonyPrefix]
+    public static bool Prefix(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, bool firstEvent, ref EnumHandHandling handHandling)
+    {
+        if (blockSel == null || byEntity == null)
+        {
+            return true;
+        }
+
+        bool pressedCtrlKey = byEntity.Controls.CtrlKey;
+        CollectibleBehaviorGroundStorable behavior = byEntity.ActiveHandItemSlot?.GetGroundStorableBehavior();
+        bool requiresCtrlKey = behavior?.StorageProps.CtrlKey == true;
+
+        if (behavior == null || !behavior.CanFixPlacement() || (requiresCtrlKey && !pressedCtrlKey))
+        {
+            return true;
+        }
+
+        EnumHandling _handling = EnumHandling.PassThrough;
+
+        behavior.OnHeldInteractStart(slot, byEntity, blockSel, null, true, ref handHandling, ref _handling);
+
+        return false;
+    }
+}
