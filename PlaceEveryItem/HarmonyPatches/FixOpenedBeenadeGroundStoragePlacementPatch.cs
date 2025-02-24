8950beb57696682dd2f02c999213c9813520678f
Dana <beware.14b@gmail.com>
2025-02-24 19:19:50 +0200

    fix beenade placement

---
+++ PlaceEveryItem/HarmonyPatches/FixOpenedBeenadeGroundStoragePlacementPatch.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+
+namespace PlaceEveryItem;
+
+[HarmonyPatch(typeof(ItemOpenedBeenade), nameof(ItemOpenedBeenade.OnHeldInteractStart))]
+public static class FixOpenedBeenadeGroundStoragePlacementPatch
+{
+    [HarmonyPrefix]
+    public static bool Prefix(ItemSlot itemslot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, bool firstEvent, ref EnumHandHandling handling)
+    {
+        if (blockSel == null || byEntity == null)
+        {
+            return true;
+        }
+
+        bool pressedCtrlKey = byEntity.Controls.CtrlKey;
+        CollectibleBehaviorGroundStorable behavior = byEntity.ActiveHandItemSlot?.GetGroundStorableBehavior();
+        bool requiresCtrlKey = behavior?.StorageProps.CtrlKey == true;
+
+        if (behavior == null || !behavior.CanFixPlacement() || (requiresCtrlKey && !pressedCtrlKey))
+        {
+            return true;
+        }
+
+        EnumHandling _handling = EnumHandling.PassThrough;
+
+        behavior.OnHeldInteractStart(itemslot, byEntity, blockSel, null, true, ref handling, ref _handling);
+
+        return false;
+    }
+}
