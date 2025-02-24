ca829758f350881591ce8a4939e8705144bebddf
Dana <beware.14b@gmail.com>
2025-02-24 18:35:51 +0200

    make tree seeds ground storable

---
+++ PlaceEveryItem/HarmonyPatches/FixTreeSeedGroundStoragePlacementPatch.cs
@@ -0,0 +1,33 @@
+using HarmonyLib;
+using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+
+namespace PlaceEveryItem;
+
+[HarmonyPatch(typeof(ItemTreeSeed), nameof(ItemTreeSeed.OnHeldInteractStart))]
+public static class FixTreeSeedGroundStoragePlacementPatch
+{
+    [HarmonyPrefix]
+    public static bool Prefix(ItemSlot itemslot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, bool firstEvent, ref EnumHandHandling handHandling)
+    {
+        if (blockSel == null || byEntity == null)
+        {
+            return true;
+        }
+
+        bool pressedCtrlKey = byEntity.Controls.CtrlKey;
+        CollectibleBehaviorGroundStorable behavior = byEntity.ActiveHandItemSlot?.GetGroundStorableBehavior();
+        bool requiresCtrlKey = behavior?.StorageProps.CtrlKey == true;
+
+        if (behavior == null || !behavior.CanFixPlacement() || (requiresCtrlKey && !pressedCtrlKey))
+        {
+            return true;
+        }
+
+        EnumHandling handling = EnumHandling.PassThrough;
+
+        behavior.OnHeldInteractStart(itemslot, byEntity, blockSel, null, true, ref handHandling, ref handling);
+
+        return false;
+    }
+}
