272692de754ddb066e9d3a7c069d88ae7e2cf017
Dana <beware.14b@gmail.com>
2025-02-24 20:02:40 +0200

    fix drygrass placement

---
+++ PlaceEveryItem/HarmonyPatches/FixDryGrassGroundStoragePlacementPatch.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+
+namespace PlaceEveryItem;
+
+[HarmonyPatch(typeof(ItemDryGrass), nameof(ItemDryGrass.OnHeldInteractStart))]
+public static class FixDryGrassGroundStoragePlacementPatch
+{
+    [HarmonyPrefix]
+    public static bool Prefix(ItemSlot itemslot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, bool firstEvent, ref EnumHandHandling handHandling)
+    {
+        if (blockSel == null || byEntity == null)
+        {
+            return true;
+        }
+
+        bool pressedCtrlKey = byEntity.Controls.CtrlKey;
+        CollectibleBehaviorGroundStorable behavior = byEntity.ActiveHandItemSlot?.GetGroundStorableBehavior();
+        bool requiresCtrlKey = behavior?.StorageProps.CtrlKey == true;
+
+        if (behavior == null || !behavior.CanFixPlacement() || (requiresCtrlKey && !pressedCtrlKey))
+        {
+            return true;
+        }
+
+        EnumHandling handling = EnumHandling.PassThrough;
+
+        behavior.OnHeldInteractStart(itemslot, byEntity, blockSel, null, true, ref handHandling, ref handling);
+
+        return false;
+    }
+}
